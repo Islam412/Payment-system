296f16f777b3c15e9a61945fb8f80b537b6dda12
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-12 01:42:50 +0200

    crate register function p1

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
+from .models import User
+from .forms import UserRegisterForm
+
 # Create your views here.
+
+
+def register(request):
+    return render(request,'userauths/sign-up.html')
