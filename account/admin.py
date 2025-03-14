3dad1d3d69435a3ce1ebfa06e54d1fd766a85064
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-14 18:10:11 +0200

    call KYC at admin

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from account.models import Account
+from account.models import Account , KYC
 from userauths.models import User
 from import_export.admin import ImportExportModelAdmin
 
@@ -14,5 +14,11 @@ class AccountAdminModel(ImportExportModelAdmin):
     search_fields = ['user__username', 'user__email', 'account_number']
 
 
+class KYCAdmin(ImportExportModelAdmin):
+    search_fields = ["full_name"]
+    list_display = ['user', 'full_name', 'gender', 'identity_type', 'date_of_birth'] 
+
+
 
 admin.site.register(Account, AccountAdminModel)
+admin.site.register(KYC, KYCAdmin)
