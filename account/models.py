18db00009722fa87fd43e8aa385e522c560976a8
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-14 15:30:47 +0200

    create user direct path for save file id

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+def user_directory_path(instance, filename):
+    ext = filename.split(".")[-1]
+    filename = "%s_%s" % (instance.id, ext)
+    return "user_{0}/{1}".format(instance.user.id, filename)
