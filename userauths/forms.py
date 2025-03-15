cfe9d23f534aab8fa8abb548fb50e5d9d72c46a0
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-15 11:29:11 +0200

    fix erorr

--- userauths/forms.py
+++ userauths/forms.py
@@ -5,10 +5,10 @@ from .models import User
 
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 450px; border: 2px solid #ccc; background-color: #F5F5F5; border-radius: 6px; height: 40px; padding: 10px;', 'id': "", 'placeholder':'Username'}), max_length=100, required=True)
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 450px; border: 2px solid #ccc; background-color: #F5F5F5; border-radius: 6px; height: 40px; padding: 10px;' , 'id': "", 'placeholder':'Email Address'}), required=True)
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'style': 'width: 450px; border: 2px solid #ccc; background-color: #F5F5F5; border-radius: 6px; height: 40px; padding: 10px;', 'id': "", 'placeholder':'Password'}), required=True)
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'style': 'width: 450px; border: 2px solid #ccc; background-color: #F5F5F5; border-radius: 6px; height: 40px; padding: 10px;', 'id': "", 'placeholder':'Confirm Password'}), required=True)
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Username"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder":"Email"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Confirm Password"}))
 
     class Meta:
         model = User
