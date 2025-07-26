c2ea61c1d48412c209f180b3fab56c474a1fbb0e
islam412 <islam.124.hamdy@gmail.com>
2025-07-26 23:56:20 +0300

    create Transaction models p1

--- core/models.py
+++ core/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
+from shortuuid.django_fields import ShortUUIDField
+
+from userauths.models import User
+from account.models import Account
+
+
+
 # Create your models here.
+TRANSACTION_TYPE = (
+    ("Transfer", "Transfer"),
+    ("Recieved", "Recieved"),
+    ("Withdraw", "Withdraw"),
+    ("Refund", "Refund"),
+    ("request", "Payment Request"),
+    ("none", "None")
+)
+
+TRANSACTION_STATUS = (
+    ("failed", "Failed"),
+    ("completed", "Completed"),
+    ("pending", "Pending"),
+    ("processing", "Processing"),
+    ("request_sent", "Request Sent"),
+    ("request_settled", "Request Settled"),
+    ("request_processing", "Request Processing"),
+
+)
+
+class Transaction(models.Model):
+    transaction_id = ShortUUIDField(unique=True, length=15, max_length=20, prefix="TRN")
+    
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+    
+    reciever = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="reciever")
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="sender")
+   
+    reciever_account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name="reciever_account")
+    sender_account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name="sender_account")
+    
+    status = models.CharField(choices=TRANSACTION_STATUS, max_length=100, default="pending")
+    transaction_type = models.CharField(choices=TRANSACTION_TYPE, max_length=100, default="none")
+    
+    date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    
+    def __str__(self):
+        try:
+            return f"{self.user}"
+        except:
+            return f"Transaction"
