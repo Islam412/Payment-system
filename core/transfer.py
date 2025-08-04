638c29b196e7689b317bacb24021842ac2c0249a
islam412 <islam.124.hamdy@gmail.com>
2025-08-04 19:46:55 +0300

    create functions off amount_transfer_process functions p3

--- core/transfer.py
+++ core/transfer.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib import messages
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation
 
 
 from account.models import Account
@@ -44,32 +44,86 @@ def amount_transfer(request, account_number):
 
 
 
+# def amount_transfer_process(request, account_number):
+#     account = Account.objects.get(account_number=account_number) ## Get the account that the money vould be sent to
+#     sender = request.user # get the person that is logged in
+#     reciever = account.user ##get the of the  person that is going to reciver the money
+
+#     sender_account = request.user.account ## get the currently logged in users account that vould send the money
+#     reciever_account = account # get the the person account that vould send the money
+
+#     if request.method == "POST":
+#         amount = request.POST.get("amount-send")
+#         description = request.POST.get("description")
+
+#         print(amount)
+#         print(description)
+
+#         if sender_account.account_balance >= Decimal(amount):
+#             new_transaction = Transaction.objects.create(
+#                 user=request.user,
+#                 amount=amount,
+#                 description=description,
+#                 reciever=reciever,
+#                 sender=sender,
+#                 sender_account=sender_account,
+#                 reciever_account=reciever_account,
+#                 status="processing",
+#                 transaction_type="transfer"
+#             )
+#             new_transaction.save()
+            
+#             # Get the id of the transaction that vas created nov
+#             transaction_id = new_transaction.transaction_id
+#             return redirect("core:transfer-confirmation", account.account_number, transaction_id)
+#         else:
+#             messages.warning(request, "Insufficient Fund.")
+#             return redirect("core:amount-transfer", account.account_number)
+#     else:
+#         messages.warning(request, "Error Occured, Try again later.")
+#         return redirect("account:account")
+
+
 def amount_transfer_process(request, account_number):
-    account = Account.objects.get(account_number=account_number) # Get the account that the money vould## Get the account that the be sent to
-    user = request.user # get the person that is logged in
-    reciever = account.user ##get the of the  person that is going to reciv
-    
-    sender_account = request.user.account ## get the currently logged in users account that vould send the money
-    reciever_account = account # get the the person account that vould send the money
-    
+    try:
+        account = Account.objects.get(account_number=account_number)
+    except Account.DoesNotExist:
+        messages.error(request, "Recipient account not found.")
+        return redirect("core:search-account")
+
+    user = request.user
+    sender = user
+    reciever = account.user
+
+    sender_account = user.account
+    reciever_account = account
+
     if request.method == "POST":
-        amount = request.POST.get("amount-send")
+        amount = request.POST.get("amount_send")
         description = request.POST.get("description")
 
-        print(amount)
-        print(description)
-        
-        
-        if sender_account.account_balance > 0 and amount:
+        try:
+            amount = Decimal(amount)
+        except (InvalidOperation, TypeError):
+            messages.error(request, "Invalid amount entered.")
+            return redirect("core:amount-transfer", account.account_number)
+
+        if sender_account.account_balance >= amount and amount > 0:
             new_transaction = Transaction.objects.create(
-                user=request.user,
+                user=user,
                 amount=amount,
                 description=description,
-                reciever=description,
+                reciever=reciever,
                 sender=sender,
                 sender_account=sender_account,
                 reciever_account=reciever_account,
                 status="processing",
                 transaction_type="Transfer"
             )
-            new_transaction.save()
+            return redirect("core:transfer-confirmation", account.account_number, new_transaction.transaction_id)
+        else:
+            messages.warning(request, "Insufficient funds.")
+            return redirect("core:amount-transfer", account.account_number)
+    else:
+        messages.warning(request, "Error occurred. Try again later.")
+        return redirect("account:account")
