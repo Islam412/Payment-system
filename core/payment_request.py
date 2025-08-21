f41b4957d9b964edcec10acd648b2c8164e33261
islam412 <islam.124.hamdy@gmail.com>
2025-08-21 23:40:31 +0300

    desing ammount request confirmations functions p1

--- core/payment_request.py
+++ core/payment_request.py
@@ -76,7 +76,19 @@ def amount_request_process(request, account_number):
     else:
         messages.warning(request, "Error Occured, try again later.")
         return redirect("account:dashboard")
+
+
+
+def amount_request_onfirmation(request, account_number, transaction_id):
+    account = Account.objects.get(account_number=account_number)
+    transaction = Transaction.objects.get(transaction_id=transaction_id)
+
+    context = {
+        "account":account,
+        "transaction":transaction,
+    }
+    return render(request, "payment_request/amount-request-confirmation.html", context)
     
-    
+
 
 
