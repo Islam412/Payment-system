a6cd0ad857f76a4f8a8ad0a02367b1abfcbe1428
islam412 <islam.124.hamdy@gmail.com>
2025-08-22 00:41:34 +0300

    desing amount request final process url

--- core/urls.py
+++ core/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('amount-request/<account_number>/', payment_request.amount_request, name='amount-request'),
     path('amount-request-process/<account_number>/', payment_request.amount_request_process, name='amount-request-process'),
     path('amount-request-confirmation/<account_number>/<transaction_id>/', payment_request.amount_request_confirmation, name='amount-request-confirmation'),
+    path('amount-request-final-process/<account_number>/<transaction_id>/', payment_request.amount_request_final_process, name='amount-request-final-process'),
 ]
