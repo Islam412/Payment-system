dc403829c747c47ff3777ffdd8d556308d448052
islam412 <islam.124.hamdy@gmail.com>
2025-08-21 23:43:04 +0300

    desing ammount request confirmations url

--- core/urls.py
+++ core/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('request-search-account/', payment_request.search_users_request, name='request-search-account'),
     path('amount-request/<account_number>/', payment_request.amount_request, name='amount-request'),
     path('amount-request-process/<account_number>/', payment_request.amount_request_process, name='amount-request-process'),
+    path('amount-request-confirmation/<account_number>/', payment_request.amount_request_confirmation, name='amount-request-confirmation'),
 ]
