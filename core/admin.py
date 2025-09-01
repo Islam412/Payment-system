0c282666e9ccca60079016de2593d11f05dc8a98
islam412 <islam.124.hamdy@gmail.com>
2025-09-01 13:36:23 +0300

    desig credit card admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from core.models import Transaction
+from core.models import Transaction , CreditCard
 
 
 # Register your models here.
@@ -9,6 +9,12 @@ class TransactionAdmin(admin.ModelAdmin):
     list_display = ['amount' , 'status' , 'transaction_type' , 'reciever' , 'sender']
     list_display = ['user' , 'amount' , 'status' , 'transaction_type' , 'reciever' , 'sender']
 
+
+
+
+class CreditCardAdmin(admin.ModelAdmin):
+    list_display = ['amount' , 'card_type']
+    list_display = ['user' , 'amount' , 'card_type']
 
 
 
