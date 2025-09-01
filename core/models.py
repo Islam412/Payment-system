a69d55c86199f3c3ff70472dc59c922956222471
islam412 <islam.124.hamdy@gmail.com>
2025-09-01 13:31:14 +0300

    desing credit card model

--- core/models.py
+++ core/models.py
@@ -27,6 +27,13 @@ TRANSACTION_STATUS = (
     ("request_processing", "Request Processing"),
 )
 
+CARD_TYPE = (
+    ("Master", "Master"),
+    ("Visa", "Visa"),
+    ("Verve", "Verve"),
+
+)
+
 class Transaction(models.Model):
     transaction_id = ShortUUIDField(unique=True, length=15, max_length=20, prefix="TRN")
     
@@ -51,3 +58,24 @@ class Transaction(models.Model):
             return f"{self.user}"
         except:
             return f"Transaction"
+        
+
+
+class CreditCard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card_id = ShortUUIDField(unique=True, length=5, max_length=20, prefix="CARD", alphabet="1234567890")
+
+    name = models.CharField(max_length=100)
+    number = models.IntegerField()
+    month = models.IntegerField()
+    year = models.IntegerField()
+    cvv = models.IntegerField()
+
+    amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+
+    card_type = models.CharField(choices=CARD_TYPE, max_length=20, default="Master")
+
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}"
