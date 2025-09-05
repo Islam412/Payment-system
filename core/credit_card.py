55e323f76428fe8beb4948ea5b9e0cf95f85c297
islam412 <islam.124.hamdy@gmail.com>
2025-09-05 15:21:13 +0300

    desing fund credit card views p1

--- core/credit_card.py
+++ core/credit_card.py
@@ -2,9 +2,8 @@ from django.shortcuts import render , redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from decimal import Decimal
 
-
-from core.forms import CreditCardForm
 from core.models import CreditCard
 from account.models import Account
 
@@ -20,3 +19,27 @@ def card_detail(request, card_id):
     }
 
     return render(request, 'credit_card/card-detail.html', context)
+
+
+
+def fund_credit_card(request, card_id):
+    credit_card = CreditCard.objects.get(card_id=card_id, user=request.user)
+    account = request.user.account
+
+    if request.method == "POST":
+        amount = request.POST.get("funding_amount")
+
+        if Decimal(amount) <= account.account_balance:
+            account.account_balance -= Decimal(amount)
+            account.save()
+
+            credit_card.amount += Decimal(amount)
+            credit_card.save()
+
+            messages.success(request, "Funding Successful")
+            return redirect('core:card-detail',credit_card.card_id)
+        else:
+            messages.warning(request, "Insufficient Funds")
+            return redirect("core:card-detail", credit_card.card_id)
+
+
