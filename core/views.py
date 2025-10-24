696b4ed4427d8ef4b81c35a3910afd4e71c68dd5
Islam412 <islam.124.hamdy@gmail.com>
2025-10-24 21:07:45 +0300

    desing home views p1

--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+
+from .models import Home
+
 
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'core/home.html')
+class HomeView(DetailView):
+    model = Home
+    template_name = 'core/home.html'
+    context_object_name = 'home'
+
+    def get_object(self):
+        return Home.objects.first()
+
+# def home(request):
+#     return render(request, 'core/home.html')
