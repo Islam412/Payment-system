47bcd1c6351226767fe48a6fbd222e79bfd0abd6
Islam412 <islam.124.hamdy@gmail.com>
2025-10-30 17:22:59 +0300

    add translate at kyc model

--- account/models.py
+++ account/models.py
@@ -78,27 +78,27 @@ IDENTITY_TYPE = (
 )
 
 class KYC(models.Model):
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
-    user =  models.OneToOneField(User, on_delete=models.CASCADE)
-    account =  models.OneToOneField(Account, on_delete=models.CASCADE, null=True, blank=True)
-    full_name = models.CharField(max_length=1000)
-    image = models.ImageField(upload_to="kyc", default="default.jpg")
-    marrital_status = models.CharField(choices=MARITAL_STATUS, max_length=40)
-    gender = models.CharField(choices=GENDER, max_length=40)
-    identity_type = models.CharField(choices=IDENTITY_TYPE, max_length=140)
-    identity_image = models.ImageField(upload_to="kyc", null=True, blank=True)
-    date_of_birth = models.DateTimeField(auto_now_add=False)
-    signature = models.ImageField(upload_to="kyc")
+    id = models.UUIDField(_('id'),primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    user =  models.OneToOneField(User, on_delete=models.CASCADE,verbose_name=_('user'))
+    account =  models.OneToOneField(Account, on_delete=models.CASCADE, null=True, blank=Trueverbose_name=_('account'))
+    full_name = models.CharField(_('full name'),max_length=1000)
+    image = models.ImageField(_('image'),upload_to="kyc", default="default.jpg")
+    marrital_status = models.CharField(_('marrital status'),choices=MARITAL_STATUS, max_length=40)
+    gender = models.CharField(_('gender'),choices=GENDER, max_length=40)
+    identity_type = models.CharField(_('identity type'),choices=IDENTITY_TYPE, max_length=140)
+    identity_image = models.ImageField(_('identity image'),upload_to="kyc", null=True, blank=True)
+    date_of_birth = models.DateTimeField(_('date of birth'),auto_now_add=False)
+    signature = models.ImageField(_('signature'),upload_to="kyc")
 
     # Address
-    country = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
+    country = models.CharField(_('country'),max_length=100)
+    state = models.CharField(_('state'),max_length=100)
+    city = models.CharField(_('city'),max_length=100)
 
     # Contact Detail
-    mobile = models.CharField(max_length=1000)
-    fax = models.CharField(max_length=1000)
-    date = models.DateTimeField(auto_now_add=True)
+    mobile = models.CharField(_('mobile'),max_length=1000)
+    fax = models.CharField(_('fax'),max_length=1000)
+    date = models.DateTimeField(_('date'),auto_now_add=True)
 
 
     def __str__(self):
