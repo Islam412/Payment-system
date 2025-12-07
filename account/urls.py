066e32b016eb0d0c8bd6c905d980d247c3b68287
Islam412 <islam.124.hamdy@gmail.com>
2025-12-07 21:55:46 +0200

    create urls at api

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from account import views
+from .api import AccountDetailAPI, KYCSubmitAPI
 
 
 app_name = 'account'
@@ -10,4 +11,8 @@ urlpatterns = [
     path('kyc/', views.kyc_registration, name='kyc-registration'),
     path('', views.account, name='account'),
     path('dashboard/', views.dashboard, name='dashboard'),
+
+    #api
+    path("api/account/", AccountDetailAPI.as_view(), name="api-account-detail"),
+    path("api/kyc/", KYCSubmitAPI.as_view(), name="api-kyc-submit"),
 ]
