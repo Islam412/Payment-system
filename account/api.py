787264edc281f884a71d90911680922896f31aa1
Islam412 <islam.124.hamdy@gmail.com>
2025-12-07 21:52:53 +0200

    desing KYCSubmitAPI

--- account/api.py
+++ account/api.py
@@ -7,6 +7,9 @@ from .models import Account, KYC
 from .serializers import AccountSerializer, KYCSerializer
 
 
+
+
+
 class AccountDetailAPI(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -17,3 +20,36 @@ class AccountDetailAPI(APIView):
 
 
 
+
+
+class KYCSubmitAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """Return KYC details if exists"""
+        try:
+            kyc = KYC.objects.get(user=request.user)
+            serializer = KYCSerializer(kyc)
+            return Response(serializer.data)
+        except KYC.DoesNotExist:
+            return Response({"detail": "KYC not submitted yet"}, status=404)
+
+    def post(self, request):
+        """Submit or update KYC form"""
+        user = request.user
+        account = Account.objects.get(user=user)
+
+        try:
+            kyc = KYC.objects.get(user=user)
+            serializer = KYCSerializer(kyc, data=request.data, partial=True)
+        except KYC.DoesNotExist:
+            serializer = KYCSerializer(data=request.data)
+
+        if serializer.is_valid():
+            kyc_obj = serializer.save(user=user, account=account)
+            return Response({
+                "message": "KYC Submitted Successfully",
+                "data": KYCSerializer(kyc_obj).data
+            }, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
