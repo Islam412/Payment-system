65ecb0788ddcd8c677cd965370480d242787f2e1
Islam412 <islam.124.hamdy@gmail.com>
2025-12-10 15:23:35 +0200

    create notification processor p1

--- core/company_contexet_processor.py
+++ core/company_contexet_processor.py
@@ -1,6 +1,21 @@
-from .models import Company
+from .models import Company , Notification
 
 
 def get_company_data(request):
     data = Company.objects.last()
     return {'company_data': data }
+
+
+
+def notifications_processor(request):
+    if request.user.is_authenticated:
+        notifications = Notification.objects.filter(user=request.user).order_by('-date')[:10]
+        unread_count = Notification.objects.filter(user=request.user, is_read=False).count()
+    else:
+        notifications = []
+        unread_count = 0
+
+    return {
+        "notifications": notifications,
+        "unread_count": unread_count,
+    }
