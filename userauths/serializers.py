d9cc4771123d61d91c9ad64f1f1182c7dd053d76
Islam412 <islam.124.hamdy@gmail.com>
2025-12-23 02:21:26 +0200

    desing login seralizer

--- userauths/serializers.py
+++ userauths/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
+
 from .models import User
 
 
@@ -30,4 +32,23 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
 
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        try:
+            user_obj = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User does not exist")
+
+        user = authenticate(email=email, password=password)
+
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+
+        attrs["user"] = user
+        return attrs
