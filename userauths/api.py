3c9b9a439b618d333c2508085409ec7cf03dc110
Islam412 <islam.124.hamdy@gmail.com>
2025-12-23 02:07:08 +0200

    desing register api view

---
+++ userauths/api.py
@@ -0,0 +1,30 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+from .serializers import RegisterSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            # Token (لو شغال بـ Token Auth)
+            token, created = Token.objects.get_or_create(user=user)
+
+            return Response({
+                "message": "تم إنشاء الحساب بنجاح",
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                },
+                "token": token.key
+            }, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
