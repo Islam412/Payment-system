0b1f268fd991ae8d5678ef2a6c2657f75937566e
Islam412 <islam.124.hamdy@gmail.com>
2025-12-24 22:31:00 +0200

    desing contact us api urls

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from core import views , transfer , transaction , payment_request , credit_card 
 from .views import HomeView , contatct_us , need_help , about_us , create_notification
-from .api import CreditCardDetailAPIView , FundCreditCardAPIView , WithdrawCreditCardAPIView , DeleteCreditCardAPIView , SearchUsersRequestAPIView , AmountRequestAPIView , AmountRequestProcessAPIView , AmountRequestConfirmationAPIView , AmountRequestFinalProcessAPIView , AmountRequestCompletedAPIView , SettlementConfirmationAPIView , SettlementProcessingAPIView , SettlementCompletedAPIView , DeletePaymentRequestAPIView , TransactionListAPIView , TransactionDetailAPIView , SearchUsersAccountNumberAPIView , AmountTransferAPIView , AmountTransferProcessAPIView , TransferConfirmationAPIView , TransferFinalProcessAPIView , TransferCompletedAPIView , HomeAPIView
+from .api import CreditCardDetailAPIView , FundCreditCardAPIView , WithdrawCreditCardAPIView , DeleteCreditCardAPIView , SearchUsersRequestAPIView , AmountRequestAPIView , AmountRequestProcessAPIView , AmountRequestConfirmationAPIView , AmountRequestFinalProcessAPIView , AmountRequestCompletedAPIView , SettlementConfirmationAPIView , SettlementProcessingAPIView , SettlementCompletedAPIView , DeletePaymentRequestAPIView , TransactionListAPIView , TransactionDetailAPIView , SearchUsersAccountNumberAPIView , AmountTransferAPIView , AmountTransferProcessAPIView , TransferConfirmationAPIView , TransferFinalProcessAPIView , TransferCompletedAPIView , HomeAPIView , ContactUsAPIView
 
 app_name = 'core'
 
@@ -78,6 +78,7 @@ urlpatterns = [
     path("api/transfer-final/<str:account_number>/<str:transaction_id>/", TransferFinalProcessAPIView.as_view(), name="transfer-final-api"),
     path("api/transfer-completed/<str:account_number>/<str:transaction_id>/", TransferCompletedAPIView.as_view(), name="transfer-completed-api"),
 
-    # home api
+    # home api & contact-us api 
     path("api/", HomeAPIView.as_view(), name="home-api"),
+    path("api/contact-us/", ContactUsAPIView.as_view(), name="contact-us-api"),
 ]
