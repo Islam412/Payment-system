ec3488168f7c71f58d5d0bdd7ca6aacfa6e31642
Islam412 <islam.124.hamdy@gmail.com>
2026-06-13 02:05:51 +0300

    fix ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2lnt5w73elp2mvfs7@cie$_b^zs96=kmn#(tu-y(wb1f9xnj-+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'payment-system-production-bc6b.up.railway.app']
 
 
 # Application definition
